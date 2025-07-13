080f68d56343b9007de5f9257eb3e73729131fd7
ComoSans <153391626+ComoSans@users.noreply.github.com>
2025-07-13 16:15:44 -0400

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Como Sans'
+description 'Replacements for GTA Game Files'
+version '1.0.0'
+
+this_is_a_map 'yes'
+
+--any issues, send me a message on discord comosans
